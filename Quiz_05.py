fb844c5f800e37ca8bf84bac57621c41ea3b2857
BAEJUNGHUN2018066 <144421342+BAEJUNGHUN2018066@users.noreply.github.com>
2023-10-14 01:06:12 +0900

    Create Quiz_05.py
    7주차 과제입니다.

---
+++ Quiz_05.py
@@ -0,0 +1,25 @@
+class 붕어빵:
+    def __init__(self, name, money):
+        self.name = name
+        self.money = money
+        self.total = 0
+    def sell(self):
+        print(f"{self.name}을 {self.money}에 팔았습니다")
+        self.total += self.money
+    def eat(self):
+        print(self.name + "을 먹고있습니다.")
+
+
+cream = 붕어빵("슈크림", 700)
+redbean= 붕어빵("팥", 500)
+eggbread =붕어빵("계란빵", 700)
+
+cream.sell()
+cream.sell()
+redbean.sell()
+eggbread.sell()
+eggbread.sell()
+
+total = (f"{cream.total + redbean.total + eggbread.total}")
+
+print(total)
